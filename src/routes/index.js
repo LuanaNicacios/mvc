b3e4fea1d730b5eac90edeff777c1a8207d31b15
NIK4CI0 <luanavsnicacio@gmail.com>
2023-06-28 16:47:55 -0300

    updateMVC

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,13 @@
 /*PAGINA DE ABERTURA*/
+import Router from 'express';
+
+const router = Router(); // função usada no express que roteia os caminhos utilizados na API //
+
+router.get("/", function (req, res) {       //A barra indica que é a página padrão (chamada de end point), vai sempre mostrar a mensagem de boas vindas. Ex: blablabla.com.br/ //
+    res.status(200).send({                  // res = status = resposta da requisição //
+        title: "Hello World! n/Bem vindas a turma Devas do Full Stack!",
+        version: "1.0.0"
+    })
+})
+
+export default router;
